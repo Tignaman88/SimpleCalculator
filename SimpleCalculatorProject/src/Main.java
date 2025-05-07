2849c6cadd2c59c1548f521c08cd8962fc14c280
Tignaman88 <alessioscarpello2809@gmail.com>
2025-05-07 17:53:15 +0100

    Basic Classes Created

---
+++ SimpleCalculatorProject/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        SimpleCalculator operation = new SimpleCalculator();
+
+        System.out.println(operation.addition(4, 7));
+    }
+}
